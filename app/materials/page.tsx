5cb2b1479f33495efb4ca6edbda63c428a968941
71alex71-dev <71alex71@gmail.com>
2026-05-24 14:16:55 +0300

    Polish Avito generator price input behavior hint

--- app/materials/page.tsx
+++
@@ -1,5 +0,0 @@
-import CalculatorApp from '../components/CalculatorApp';
-
-export default function MaterialsPage() {
-  return <CalculatorApp page="materials" />;
-}
--- app/new-calculation/page.tsx
+++
@@ -1,5 +0,0 @@
-import CalculatorApp from '../components/CalculatorApp';
-
-export default function NewCalculationPage() {
-  return <CalculatorApp page="new" />;
-}
