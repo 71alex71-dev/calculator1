fd3814236baa8d4f3056001ae22d3dedcbcce7f8
71alex71-dev <71alex71@gmail.com>
2026-05-24 14:08:35 +0300

    Remove legacy calculator routes and keep only Avito generator

--- app/client-message/page.tsx
+++
@@ -1,5 +0,0 @@
-import CalculatorApp from '../components/CalculatorApp';
-
-export default function ClientMessagePage() {
-  return <CalculatorApp page="message" />;
-}
--- app/summary/page.tsx
+++
@@ -1,5 +0,0 @@
-import CalculatorApp from '../components/CalculatorApp';
-
-export default function SummaryPage() {
-  return <CalculatorApp page="summary" />;
-}
